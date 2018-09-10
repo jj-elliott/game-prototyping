6c24958728ccd3758d99ce5a2d7215441f10d076
uscitp-ihsiukao <ihsiukao@usc.edu>
2018-09-09 17:06:27 -0700

    Added camera movement with WASD keys

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float Speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, Speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -Speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(-Speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(0, Speed, 0);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Translate(0, -Speed, 0);
+        }
+    }
+}
