b384ed152760dc53177f2864c00aafd2b5818d26
scubasteve95128 <asuquo@usc.edu>
2018-09-12 14:25:29 -0700

    Added base scripts for unit and unit weapons

---
+++ Assets/Scripts/UnitWeapon.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UnitWeapon : MonoBehaviour
+{
+    [SerializeField]
+    protected float MaxRange;
+
+    [SerializeField]
+    protected float MinRange;
+
+    [SerializeField]
+    protected float Damage;
+
+    [SerializeField]
+    protected UnitBase unit;
+
+	// Use this for initialization
+	protected virtual void Start()
+    {
+        if (unit == null)
+        {
+            unit = GetComponentInParent<UnitBase>();
+        }
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update()
+    {
+		if (unit != null)
+        {
+            Transform t = unit.GetWeaponTarget();
+            if (t != null && CanFire(t.position))
+            {
+                Fire(t);
+            }
+        }
+	}
+
+    protected virtual bool CanFire(Vector3 targetPos)
+    {
+        if (unit != null)
+        {
+            float distToTarget = (targetPos - unit.transform.position).magnitude;
+            return distToTarget > MinRange && distToTarget < MaxRange;
+        }
+
+        return false;
+    }
+
+    protected abstract void Fire(Transform target);
+}
